56f9583bd7d63b4c0406dc95ff63a78796f51f45
fabiola-alston <f.alston.1@estudiantec.cr>
2024-08-01 21:14:42 -0600

    Created project (deleted old one due to project type being incorrect) and nearly finished GUI.

---
+++ WhatSocket/WhatSocket/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WhatSocket
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chatBox = new System.Windows.Forms.RichTextBox();
+            this.hostPortBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.addressPortBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter your message: ";
+            // 
+            // chatBox
+            // 
+            this.chatBox.CausesValidation = false;
+            this.chatBox.Location = new System.Drawing.Point(214, 35);
+            this.chatBox.Name = "chatBox";
+            this.chatBox.Size = new System.Drawing.Size(308, 341);
+            this.chatBox.TabIndex = 3;
+            this.chatBox.Text = "";
+            // 
+            // hostPortBox
+            // 
+            this.hostPortBox.Location = new System.Drawing.Point(71, 101);
+            this.hostPortBox.Name = "hostPortBox";
+            this.hostPortBox.Size = new System.Drawing.Size(70, 20);
+            this.hostPortBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(12, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Your port:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(12, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Enter port address:";
+            // 
+            // addressPortBox
+            // 
+            this.addressPortBox.Location = new System.Drawing.Point(118, 133);
+            this.addressPortBox.Name = "addressPortBox";
+            this.addressPortBox.Size = new System.Drawing.Size(70, 20);
+            this.addressPortBox.TabIndex = 7;
+            this.addressPortBox.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(534, 511);
+            this.Controls.Add(this.addressPortBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.hostPortBox);
+            this.Controls.Add(this.chatBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "WhatSocket";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox chatBox;
+        private System.Windows.Forms.TextBox hostPortBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox addressPortBox;
+    }
+}
+
---
+++ WhatSocket/WhatSocket/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhatSocket
+{
+    public partial class Form1 : Form
+    {
+        private int hostPort = 5000;
+        private int addressPort;
+        private string addressPortString;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // chat box is non editable
+            chatBox.Enabled = false;
+            chatBox.ReadOnly = true;
+
+            // port home box is non editable
+            hostPortBox.Enabled = false;
+            hostPortBox.ReadOnly = true;
+
+            // adds own port to host port box
+            hostPortBox.Text = hostPort.ToString();
+            
+        }
+
+        private void sendMessage()
+        {
+            string input = textBox1.Text;
+            chatBox.AppendText($"You ({hostPort}): {input}" + "\r\n");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sendMessage();
+      
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            addressPortString = addressPortBox.Text;
+            addressPort = Convert.ToInt32(addressPortBox.Text);
+        }
+    }
+}
